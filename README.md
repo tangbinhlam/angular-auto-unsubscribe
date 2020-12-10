60229e0ba1d59e1f124696715ef1a62155323214
osalam <tangbinhlam@gmail.com>
2020-12-10 15:29:53 -0800

    Resolve the problem with  unsubscribe() method

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.8.
 
 ## Development Unsubscibe for avoid Memory Leak in Angular
+The problem happend when you forgot unsubscribe that cause the memory leak problem
+
+### 1: Use Unsubcribe with method unsubscribe()
+```
+  ngOnDestroy(): void {
+    this.subcription$.unsubscribe();
+  }
+```
--- src/app/unsubscribe-method/unsubscribe-method.component.ts
+++ src/app/unsubscribe-method/unsubscribe-method.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, Subscribable, Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-unsubscribe-method',
   templateUrl: './unsubscribe-method.component.html',
   styleUrls: ['./unsubscribe-method.component.css'],
 })
-export class UnsubscribeMethodComponent implements OnInit {
+export class UnsubscribeMethodComponent implements OnInit, OnDestroy {
   subcription$: Subscription;
 
   constructor() {}
 
   ngOnInit(): void {
+    console.clear();
+    console.log('Unsubscribe Method Demo');
     this.subcription$ = interval(1000).subscribe((value) => {
       console.log(`Print ${value}`);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subcription$.unsubscribe();
+    console.log(`After call unsubscribe() method `);
   }
 }
